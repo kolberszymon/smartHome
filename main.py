52041bd95fe8a40259854ced5ce0840e8f4d6887
natalia <natalia@inuits.io>
2020-04-20 19:18:50 +0200

    main program update

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from mqtt import MQTTClient
 import machine
 from time import sleep
 # Micropython
+
+client_id='ESP8266'
+mqtt_server = '192.168.0.115'
+topic = b'test/a'
+
+def sub_cb(topic, msg): 
+   print(msg) 
+
+client = MQTTClient(client_id, mqtt_server) 
+client.connect()
 
 try:
 	switch = machine.Pin(5, machine.Pin.IN, machine.Pin.PULL_UP) # D1/GPIO5
@@ -13,10 +24,13 @@ prevValue = 0
 while True:
 	sleep(1)
 	print("s.v.: " + str( switch.value() ) )
+	client.publish(topic, msg=str(switch.value()))
 	if prevValue != switch.value():
 		if switch.value() == 1: # door opened
+			client.publish(topic, msg='State: 1, Door Opened')
 			print('Door Opened')
 		if switch.value() == 0: # door closed
+			client.publish(topic, msg='State: 0, Door Closed')
 			print('Door Closed')
 		try:
 			prevValue = switch.value()
